e34c8af144d506c27016ac5eeae0579b267fc183
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-06-30 18:27:13 -0400

    Mejorando la ejecución en paralelo del tuner.

--- Tune/ParallelForEach.cs
+++ Tune/ParallelForEach.cs
@@ -16,23 +16,19 @@ namespace Tune
 				action(items.First());
 			}
 			else {
-				var chunkSize = Environment.ProcessorCount;
-				for (var offset = 0; offset <= items.Count() / chunkSize; offset++) {
-					var chunk = items.Skip(offset * chunkSize).Take(chunkSize);
-					var resetEvents = new ManualResetEvent[chunk.Count()];
-					var currentItem = 0;
-					foreach (var item in chunk) {
-						resetEvents[currentItem] = new ManualResetEvent(false);
-						ThreadPool.QueueUserWorkItem(new WaitCallback((object data) => {
-							int methodIndex = (int) ((object[]) data)[0];
-							action((T) ((object[]) data)[1]);
-							resetEvents[methodIndex].Set();
-						}), new object[] { currentItem, item });
-						currentItem++;
-					}
-	
-					WaitHandle.WaitAll(resetEvents);
+				int itemNumber = 0;
+				ThreadPool.SetMaxThreads(Environment.ProcessorCount, Environment.ProcessorCount);
+				ManualResetEvent[] resetEvents = new ManualResetEvent[items.Count()];
+				foreach (T item in items) {
+					resetEvents[itemNumber] = new ManualResetEvent(false);
+					ThreadPool.QueueUserWorkItem(new WaitCallback((object data) => {
+						int index = (int) ((object[]) data)[0];
+						action((T) ((object[]) data)[1]);
+						resetEvents[index].Set();
+					}), new object[] { itemNumber, item });
+					itemNumber++;
 				}
+				WaitHandle.WaitAll(resetEvents);
 			}
 		}
 	}
