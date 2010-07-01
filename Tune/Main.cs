b404092f77ba711720b9522baa05d70b5c90dc78
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-07-01 19:20:13 -0400

    Implementando tuner para SS.

--- Tune/Main.cs
+++ Tune/Main.cs
@@ -9,29 +9,12 @@ namespace Tune
 		public static void Main(string[] args)
 		{
 			ITunableMetaheuristic[] algorithms = new ITunableMetaheuristic[] {
-				new GA2OptFirst4SPP(),
-				new GA4SPP(),
-				new GA2OptFirst4TSP(),
-				new GA4TSP(),
-//				new GA2OptFirst4QAP(),
-//				new GA4QAP(),
-//				new GABL2OptFirst42SP(),
-//				new GABL42SP()
-//				new GA2OptBest4QAP(),
-//				new GA2OptBest4TSP(),
-//				new GA2OptBest4SPP(),
-//				new GABL2OptBest42SP(),
-//				new GANPS42SP(),
-//				new PSO2OptFirst4QAP(),
-//				new PSO4QAP(),
-//				new PSO2OptFirst4TSP(),
-//				new PSO4TSP(),
-//				new PSOBL2OptFirst42SP(),
-//				new PSOBL42SP(),
-//				new PSO2OptBest4TSP(),
-//				new PSO2OptBestQAP(),
-//				new PSOBL2OptBest42SP(),
-//				new PSONPS42SP(),				
+				new SS4TSP(),
+				new SS2OptFirst4TSP(),
+				new SS4QAP(),
+				new SS2OptFirst4QAP(),
+				new SSBL42SP(),
+				new SSBL2OptFirst42SP(),
 			};
 			
 			algorithms.ParallelForEach(algorithm => {
@@ -69,6 +52,9 @@ namespace Tune
 					break;
 				case MetaheuristicType.PSO:
 					tuner = new PSOTuner(algorithm, instancesDir);
+					break;
+				case MetaheuristicType.SS:
+					tuner = new SSTuner(algorithm, instancesDir);
 					break;
 				default:
 					break;
---
+++ Tune/SSTuner.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+using Metaheuristics;
+
+namespace Tune
+{
+	public class SSTuner : Tuner
+	{
+		public SSTuner(ITunableMetaheuristic metaheuristic, string dirInstances)
+			: base(metaheuristic, dirInstances, 6, new int[] { 2000, 10000 }, 5)
+		{
+		}
+		
+		protected override IEnumerable<double[]> EnumerateParameters()
+		{
+            double[] timePenalties = new double[] { 250 };
+			double[] poolSizes = new double[] { 20, 40, 100, 200 };
+			double[] refSetSizes = new double[] { 4, 10 };
+			double[] explorationFactors = new double[] { 0.25, 0.5, 0.75 };
+			
+			foreach (double timePenalty in timePenalties) {
+				foreach (double poolSize in poolSizes) {
+					foreach (double refSetSize in refSetSizes) {
+						foreach (double explorationFactor in explorationFactors) {
+							yield return new double[] { timePenalty, poolSize, refSetSize, explorationFactor };
+						}
+					}
+				}
+			}
+		}
+	}
+}
