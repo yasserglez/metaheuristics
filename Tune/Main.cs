984900a57f7149922d833ceb947f0dc50f2f71a9
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-07-05 15:19:33 -0400

    Acomodando proyecto para seguir ajustando.

--- Tune/Main.cs
+++ Tune/Main.cs
@@ -8,7 +8,7 @@ namespace Tune
 	{
 		public static void Main(string[] args)
 		{
-			ITunableMetaheuristic[] algorithms = new ITunableMetaheuristic[] {
+			ITunableMetaheuristic[] algorithms = new ITunableMetaheuristic[] {		
 				#region Yasser
 //				new	ACOBL2OptBest42SP(),            
 //				new ACOBL2OptFirst42SP(),            
@@ -92,28 +92,28 @@ namespace Tune
 //				new HMTSwGRASP2OptBest4TSP(),  /* OK */          
 //				new HMTSwGRASP2OptFirst4TSP(), /* OK */           
 				
-//				new TwoOptBest4QAP(),            
-//				new TwoOptFirst4QAP(),            
+//				new TwoOptBest4QAP(),
+//				new TwoOptFirst4QAP(),
 //				new GA4QAP(),
 //				new GA2OptFirst4QAP(),
 //				new HMSAwGRASP2OptBest4QAP(),            
 //				new HMSAwGRASP2OptFirst4QAP(),            
-//				new GA2OptBest4QAP(), /* OK */   
-//				new PSO4QAP(), /* OK */
-//				new PSO2OptBest4QAP(), /* OK */
-//				new PSO2OptFirst4QAP(), /* OK */
-//				new GRASP2OptBest4QAP(), /* OK */
-//				new GRASP2OptFirst4QAP(), /* OK */    
-//				new TS4QAP(), /* OK */
-//				new HMTSwGRASP2OptBest4QAP(), /* OK */            
-//				new HMTSwGRASP2OptFirst4QAP(), /* OK */            
+//				new GA2OptBest4QAP(),
+//				new PSO4QAP(),
+//				new PSO2OptBest4QAP(),
+//				new PSO2OptFirst4QAP(),
+//				new GRASP2OptBest4QAP(),
+//				new GRASP2OptFirst4QAP(),
+//				new TS4QAP(),
+//				new HMTSwGRASP2OptBest4QAP(),
+//				new HMTSwGRASP2OptFirst4QAP(),
 				
 //				new GA2OptBest4SPP(),            
-				new GA2OptFirst4SPP(),            
-				new GRASP2OptBest4SPP(),
-				new GRASP2OptFirst4SPP(),
-				new HMTSwGRASP2OptFirst4SPP(),            
-				new HMTSwGRASP2OptBest4SPP(),       
+//				new GA2OptFirst4SPP(),            
+//				new GRASP2OptBest4SPP(),
+//				new GRASP2OptFirst4SPP(),
+//				new HMTSwGRASP2OptFirst4SPP(),            
+//				new HMTSwGRASP2OptBest4SPP(),       
 //				new HMSAwGRASP2OptBest4SPP(),            
 //				new HMSAwGRASP2OptFirst4SPP(),            
 //				new GA4SPP(), /* OK */           
