aada3f1f911d292d6e033615c9d730e2efd033bd
Ariel Hernández Amador <gnuaha7@gmail.com>
2010-07-07 16:51:42 -0400

    Arreglando un error en los MA.

--- Test/Main.cs
+++ Test/Main.cs
@@ -8,14 +8,14 @@ namespace Test
 		public static void Main(string[] args)
 		{
 		   	int time = 10000;
-			string input = "../../../Problems/TSP/Instances/rat195.in";
-			string output = "../../../Problems/TSP/Instances/rat195.out";
+			string input = "../../../Problems/QAP/Instances/sko72.in";
+			string output = "../../../Problems/QAP/Instances/sko72.out";
 			
-			(new GA4TSP()).Start(input, output+"ga", time);
-			(new SA4TSP()).Start(input, output+"sa", time);
-			(new TS4TSP()).Start(input, output+"ts", time);
-			(new ILS2OptFirst4TSP()).Start(input, output+"ils2of", time);
-			(new MA4TSP()).Start(input, output+"ma", time);
+//			(new GA4QAP()).Start(input, output+"ga", time);
+//			(new SA4QAP()).Start(input, output+"sa", time);
+//			(new TS4QAP()).Start(input, output+"ts", time);
+//			(new ILS2OptFirst4QAP()).Start(input, output+"ils2of", time);
+			(new MA4QAP()).Start(input, output+"ma", time);
 			
 		}
 	}
